179e909732abf55f880850ccff1e50125cbad332
XXXDRAVENXXX <42663095+XXXDRAVENXXX@users.noreply.github.com>
2018-08-23 19:56:51 -0700

    Initial commit

---
+++ src/chapter1/chapter1.java
@@ -0,0 +1,66 @@
+package chapter1;
+//Written by student: Kenton Vo
+
+public class chapter1 {
+	
+	public static void main(String[] args)
+	{
+		Miss();
+		Iss();
+		SSS();
+		SSS();
+		Iss();
+		SSS();
+		SSS();
+		Iss();
+		PPP();
+		PPP();
+		Iss();
+		
+		
+	}
+	public static void Iss()
+	{
+		System.out.println("IIIII");
+		int num = 0;
+		while(num<5)
+		{
+			System.out.println("  I  ");
+			num++;
+		}
+		if(num>=5)
+		{
+		System.out.println("IIIII");
+		}
+	}
+	public static void Miss()
+	{	
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+	}
+	public static void SSS()
+	{	
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+	}
+	public static void PPP()
+	{	
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+	}
+	
+}
